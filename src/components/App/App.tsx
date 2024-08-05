d07e851f19c4a4621700148b0399b07277c9e7d4
Enzo Zampaglione <71082476+EnzoZa@users.noreply.github.com>
2024-08-05 15:33:02 +0200

    img on button

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
-import Button from '../ui/Button/Button.js'
-
+import Button from '../ui/Button/Button.tsx'
 //DEMAT BREIZH
+//text="Benjamin"
 function App() {
   return <div className="App" data-testid="App"> 
-    <Button onClick={alert}>
+    <Button onClick={() => {alert("DEMAT BREIZH")}}>
       YEC'H MAT !
+    </Button>   
+    <Button onClick={() => {alert("test")}}>
+      <img src="/pacman.png"></img>
     </Button>   
   </div>  
 }
--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import style from './Button.module.css'
 
-const Button = ({children, onClick=(() => {})}: {children:string|React.ReactNode, onClick?:React.MouseEventHandler<HTMLButtonElement> | undefined}) => {
+const Button = ({children, onClick=(() => {})}: {children:React.ReactNode, onClick?:React.MouseEventHandler<HTMLButtonElement> | undefined}) => {
   return (
     <button className={style.Button} data-testid="Button" onClick={onClick}>
         {children}
     </button>
-    /* <div className="Button" data-testid="Button"> Benjamin </div> */
     )
 }
-
+/*
+const Button = (props) => {
+  console.log(props);
+  return <div className={style.Button} data-testid="Button"> {props.text} </div>;
+}
+*/
 export default Button;
