0e035bf759e32359fc7a3a3169b12dcf071cfd1f
Enzo Zampaglione <71082476+EnzoZa@users.noreply.github.com>
2024-08-07 17:18:12 +0200

    arvechal

--- src/pages/MemeThumbnail/MemeThumbnail.tsx
+++ src/pages/MemeThumbnail/MemeThumbnail.tsx
@@ -1,6 +1,7 @@
 import { ImageInterface, MemeInterface, MemeSVGViewer } from 'orsys-tjs-meme'
 import React from 'react'
 import style from './MemeThumbnail.module.css'
+import { Link } from 'react-router-dom'
 
 interface MemeThumbnailProps {
   images: ImageInterface[]
@@ -12,7 +13,12 @@ function MemeThumbnail({ images, memes }: MemeThumbnailProps) {
     <div className={style.MemeThumbnail}>
         {memes.map((meme:MemeInterface) => {
             return (
-                <MemeSVGViewer key={meme.id} meme={meme} image={images.find((item:ImageInterface) => item.id === meme.imageId)} basePath='' />
+              <Link key={meme.id} to="" style={{display:'block'}}>
+                <div> 
+                  <MemeSVGViewer meme={meme} image={images.find((item:ImageInterface) => item.id === meme.imageId)} basePath='' />
+                </div>
+              </Link>
+                
             )
         })}
     </div>
