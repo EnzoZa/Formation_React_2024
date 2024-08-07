1e5962eeb3393f4a827cad73b23186242e94f55a
Enzo Zampaglione <71082476+EnzoZa@users.noreply.github.com>
2024-08-07 17:42:23 +0200

    nav si pas trouver

--- src/pages/MemeEditor.tsx
+++ src/pages/MemeEditor.tsx
@@ -2,21 +2,24 @@ import React, { useEffect } from 'react'
 import { MemeSVGViewer } from '../components/ui/index'
 import { FlexWFirstGrow } from '../components/layout/index';
 import { MemeForm } from '../components/functionnal/index';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { MemeInterface } from 'orsys-tjs-meme';
 import { resetCurrent, changeMeme } from '../store/current';
+//Pourquoi on utilise pas .connected Ici ? 
 function MemeEditor() {
   const routeParams = useParams();
   const dispatch = useDispatch();
   const memes = useSelector((storeState:any) => storeState.ressources.memes);
+  const nav = useNavigate();
   useEffect(() => {
     if (routeParams.id !== undefined) {
       const meme = memes.find((item:MemeInterface) => item.id === parseInt(routeParams.id));
       if (meme !== undefined) {
         dispatch(changeMeme(meme));
-      } else {
-        dispatch(resetCurrent())
+      } else if (memes.length > 0) {
+        //Car il faut attendre que les memes soient chargés. Je trouve que c'est une méthode de contournement...
+        nav('/home')
       }
     } else {
       dispatch(resetCurrent())
@@ -24,7 +27,7 @@ function MemeEditor() {
     return () => {
       
     };
-  }, [routeParams])
+  }, [routeParams, memes])
   return (
     <FlexWFirstGrow>
          <MemeSVGViewer basePath='' /> { /* image={images.find((item:ImageInterface)=>item.id===meme.imageId)} meme={meme} basePath='' */ }
